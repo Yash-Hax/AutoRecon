26e7461e5a772bd25fc1e2f0e8dbdb050b529466
Yash Shendge <75063323+Yash-Hax@users.noreply.github.com>
2022-05-28 03:51:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ echo "                                  https://twitter/yash-hax"
 
 
 echo | sudo apt-get install subfinder
-echo | GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
+echo | sudo apt-get install nmap
+echo | sudo apt-get install golang
+echo | go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest0
 echo | sudo apt-get install dirsearch
+
+
 
