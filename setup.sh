5a1060fa41e5c1344f99182723446f4cb3d5bb34
Yash Hax <75063323+Yash-Hax@users.noreply.github.com>
2022-05-30 11:07:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,6 @@ echo "                                  https://twitter/yash-hax"
 
 echo | sudo apt-get install subfinder
 echo | sudo apt-get install nmap
-echo | sudo apt-get install golang
-echo | go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest0
 echo | sudo apt-get install dirsearch
 
 
